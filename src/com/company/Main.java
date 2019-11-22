dfea4c39d89bc94e58196e7f28cd29e94a32222d
Angelinodc71 <adec58@elpuig.xeill.net>
2019-11-22 12:32:58 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("/home/ivan/IdeaProjects/ExpresionesRegulares_Returns/src/com/company/santako.txt");
+        BufferedReader br = new BufferedReader(fileReader);
+
+        String line = br.readLine();
+
+        // TO CHAR ARRAY Y MAMARLA
+    }
+}
